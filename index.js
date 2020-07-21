37b9e7fb71913ee5986abea39d188cc6c8405312
dgornjakovic <danijel.gornjakovic@prodyna.com>
2020-07-21 18:54:58 +0200

    public help

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on("message", msg => {
                 if (msg.content.toLowerCase() == "!faq") {
                     msg.reply("Hi, I am Synthetix FAQ bot. I will be very happy to assist you, just ask me for **help** in DM.");
                 } else if (msg.content.toLowerCase() == "!faq help") {
-                    msg.reply("I can only answer predefined question in channel, e.g. **question 1**. \n For more commands and options send me **help** in DM");
+                    msg.reply("I can only answer a predefined question in a channel, e.g. **question 1**. \n For more commands and options send me **help** in DM");
                 } else if (msg.content.toLowerCase().startsWith("!faq question")) {
                     doQuestion(msg, "!faq question", false);
                 }
