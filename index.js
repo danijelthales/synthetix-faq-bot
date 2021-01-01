7ebf0f72bc2105e7af59cc79cfc5c608ae174b9c
dgornjakovic <danijel.gornjakovic@prodyna.com>
2021-01-01 16:13:53 +0100

    council

--- index.js
+++ index.js
@@ -172,6 +172,8 @@ var synthsMap = new Map();
 let gasSubscribersMap = new Map();
 let gasSubscribersLastPushMap = new Map();
 
+let votes = new Map();
+
 console.log("Redis URL:" + process.env.REDIS_URL);
 
 if (process.env.REDIS_URL) {
@@ -198,6 +200,14 @@ if (process.env.REDIS_URL) {
         }
     });
 
+    redisClient.get("votes", function (err, obj) {
+        votesRaw = obj;
+        if (votesRaw) {
+            votes = new Map(JSON.parse(votesRaw));
+            console.log("votes:" + votes);
+        }
+    });
+
 
 }
 
@@ -206,6 +216,7 @@ let trades = null;
 let trades100 = null;
 let trades1000 = null;
 let general = null;
+let councilChannel = null;
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.channels.fetch('785320922278133800').then(c => {
@@ -219,6 +230,9 @@ client.on("ready", () => {
     });
     client.channels.fetch('413890591840272398').then(c => {
         general = c;
+    });
+    client.channels.fetch('778445461135687733').then(c => {
+        councilChannel = c;
     });
 })
 client.on("guildMemberAdd", function (member) {
@@ -3258,7 +3272,7 @@ async function getCouncil() {
         });
         const page = await browser.newPage();
         await page.setViewport({width: 1000, height: 926});
-        await page.goto("https://council.synthetix.io/#/synthetixcouncil/proposal/QmQREHVu11dognYVbHhNSHrxeiMAzoChAsqvD68VtqAo69", {waitUntil: 'networkidle2'});
+        await page.goto("https://council.synthetix.io/#/synthetixcouncil/proposal/QmPyFrvjPRzqsxCpcUFdHU2hWGWV4EJa99ahFATtTyxyZ6", {waitUntil: 'networkidle2'});
 
         await delay(10000);
 
@@ -3406,5 +3420,99 @@ setInterval(function () {
     });
 
 }, 60 * 1000)
+
+
+let choices = new Map();
+choices.set(1, "BigPenny");
+choices.set(2, "Danijel");
+choices.set(3, "Farmwell");
+choices.set(4, "Jackson");
+choices.set(5, "Spreek");
+choices.set(6, "Synthaman");
+choices.set(7, "justwanttoknowathing");
+choices.set(8, "Farmer Joe - The DeFi Oracle");
+choices.set(9, "Psybull");
+choices.set(10, "Kaleb");
+choices.set(11, "rubber duck");
+choices.set(12, "Akin");
+choices.set(13, "Samantha");
+choices.set(14, "Michael | Framework");
+choices.set(15, "Andy | Synthetix");
+choices.set(16, "Chevis");
+choices.set(17, "Simone ");
+choices.set(18, "TerraBellus");
+choices.set(19, "larpras");
+
+
+setTimeout(function () {
+    https.get('https://hub.snapshot.page/api/synthetixcouncil/proposal/QmPyFrvjPRzqsxCpcUFdHU2hWGWV4EJa99ahFATtTyxyZ6', (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            try {
+                let results = JSON.parse(data);
+                let print = false;
+                if (votes.size > 0) {
+                    print = true;
+                }
+                for (const result in results) {
+                    if (!votes.has(result)) {
+                        let vote = results[result];
+                        let voter = vote.address;
+                        if (votes.has(voter)) {
+                            let choice = votes.get(voter);
+                            if (choice != vote.msg.payload.choice) {
+                                const exampleEmbed = new Discord.MessageEmbed();
+                                exampleEmbed.setColor("00770f");
+                                exampleEmbed.setTitle("Vote Changed");
+                                exampleEmbed.setURL("https://council.synthetix.io/#/synthetixcouncil/proposal/QmPyFrvjPRzqsxCpcUFdHU2hWGWV4EJa99ahFATtTyxyZ6");
+                                exampleEmbed.addField("From",
+                                    choices.get(choice));
+                                exampleEmbed.addField("To",
+                                    choices.get(vote.msg.payload.choice));
+                                exampleEmbed.addField("Voter",
+                                    voter);
+                                if (print) {
+                                    councilChannel.send(exampleEmbed);
+                                }
+                            }
+                        } else {
+                            const exampleEmbed = new Discord.MessageEmbed();
+                            exampleEmbed.setColor("00770f");
+                            exampleEmbed.setTitle("New Vote");
+                            exampleEmbed.setURL("https://council.synthetix.io/#/synthetixcouncil/proposal/QmPyFrvjPRzqsxCpcUFdHU2hWGWV4EJa99ahFATtTyxyZ6");
+                            exampleEmbed.addField("For",
+                                choices.get(vote.msg.payload.choice));
+                            exampleEmbed.addField("Voter",
+                                voter);
+                            if (print) {
+                                councilChannel.send(exampleEmbed);
+                            }
+                        }
+                        votes.set(voter, vote.msg.payload.choice);
+
+                        if (process.env.REDIS_URL) {
+                            redisClient.set("votes", JSON.stringify([...votes]), function () {
+                            });
+                        }
+                    }
+                }
+            } catch (e) {
+                console.log(e);
+            }
+
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
+}, 60 * 1000);
 
 
